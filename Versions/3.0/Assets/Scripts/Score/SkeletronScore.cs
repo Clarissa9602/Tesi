d230586877fe4a2bc59e87600d7153eaf8d8b4dd
Clarissa9602 <45065912+Clarissa9602@users.noreply.github.com>
2022-03-12 11:11:00 +0100

    Update 3.1

--- Versions/3.0/Assets/Scripts/Score/SkeletronScore.cs
+++ Versions/3.0/Assets/Scripts/Score/SkeletronScore.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SkeletronScore : MonoBehaviour{
+    public List<GameObject> bones;
+
     public Text scoreText;
     int score;
 
@@ -12,6 +14,25 @@ public class SkeletronScore : MonoBehaviour{
     }
 
     void Update(){
+        foreach(GameObject items in bones){
+            checkPosition(items);
+            //Debug.Log(items.name);
+        }
+        // Verifica posizione
+        Debug.Log(score);
         scoreText.text = score + "%";
+        if(score == 100)
+            LoadingManager.Instance.LoadLevel("Main Menu");
+    }
+    void checkPosition(GameObject i){
+        // Se l'item è stato trascinato nell'area corretta
+        // Fisso l'item nel posto giusto 
+        // E incremento lo score
+        if(i.name == "Cranio")
+            score += 20;
+        /*if(i.name == "Torace")
+        if(i.name == "Braccia")
+        if(i.name == "Bacino")
+        if(i.name == "Gambe")*/
     }
 }
