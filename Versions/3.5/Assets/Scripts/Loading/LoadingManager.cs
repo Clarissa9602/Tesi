95684a46fbf61c60af03a396d3ed6215d482ba42
Clarissa9602 <45065912+Clarissa9602@users.noreply.github.com>
2022-03-20 21:48:49 +0100

    Version 3.5
    Completato il primo minigioco
    Applicare le seguenti modifiche:
    - Ingrandire oggetto selezionato
    - Aggiungere gli slot left e right per braccia e gambe

---
+++ Versions/3.5/Assets/Scripts/Loading/LoadingManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadingManager: MonoBehaviour
+{
+    public static LoadingManager Instance;
+
+    public GameObject loadingScreen;
+    public float MinLoadTime;
+    public Slider slider;
+
+    public Text progressText;
+    private int progress;
+
+    void Awake(){
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        progress = 0;
+    }
+    
+    public void LoadLevel(string sceneName)
+    {
+        Debug.Log("Nome scena: "+sceneName);
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    IEnumerator LoadAsynchronously(string sceneName){
+        loadingScreen.SetActive(true);
+
+        float elapsedLoadTime = 0f;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        while (!operation.isDone)
+        {
+            elapsedLoadTime += Time.deltaTime;
+            yield return null;
+        }
+        while(elapsedLoadTime < MinLoadTime){
+            elapsedLoadTime += Time.deltaTime;
+            progress = (int)Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+            yield return null;
+        }
+        loadingScreen.SetActive(false);
+    }
+}
---
+++ Versions/3.5/Assets/Scripts/SkeletronGame/Chest.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chest : MonoBehaviour{
+   
+    [SerializeField]
+    private Transform rightPlace;
+    private Vector3 initialPosition;
+    private Vector3 mousePosition;
+    private float deltaX, deltaY;
+    public static bool locked;
+
+    void Start(){
+        initialPosition = transform.position;
+        locked = true;
+    }
+    void OnMouseDown(){
+        if(!locked){
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    void OnMouseDrag(){
+        if(!locked){
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePosition.x - deltaX,  mousePosition.y - deltaX);
+        }
+    }
+    private void OnMouseUp(){
+        if(Mathf.Abs(transform.position.x - rightPlace.position.x) <= 50f &&
+            Mathf.Abs(transform.position.y - rightPlace.position.y) <= 50f){
+                transform.position = new Vector3(rightPlace.position.x, rightPlace.position.y);
+                locked = true;
+                SkeletronScore.Instance.AddPoint();
+                disableButton();
+        }
+        else{
+            transform.position = new Vector3(initialPosition.x, initialPosition.y);
+        }
+    }
+
+    public void disableButton(){
+        GameObject.Find("btnTorace").GetComponent<Image>().color = new Color(120f, 37f, 0f, 255f);
+        GameObject.Find("btnTorace").GetComponentInParent<Button>().enabled = false;        
+    }
+    public void Unlock(){
+        locked = false;
+    }
+}
